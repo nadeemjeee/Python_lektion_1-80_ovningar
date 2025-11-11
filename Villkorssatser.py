b25fd5f2460ab4a84ed4345e74fc009e915af3b0
nadeemjeee <nadeemjeee@gmail.com>
2025-11-11 09:27:17 +0100

    Add new exercises for user input handling, including choice confirmation and player status checks

--- Villkorssatser.py
+++ Villkorssatser.py
@@ -2,13 +2,6 @@
 # Instruktion: Nu introducerar vi if, elif, else och typkonvertering (int(), float()).
 
 
-
-
-# 6.  Fråga användaren "Svara Ja eller Nej". Om svaret, oavsett versaler, är "ja", skriv ut "Du valde att fortsätta äventyret!".
-# 7.  Fråga efter en spelares status (t.ex. "redo" eller "upptagen"). Om status inte är "redo", skriv "Vänta... Spelaren är inte redo.".
-# 8.  Fråga hur många guldmynt du har (som ett tal). Om du har färre än 50, skriv ut "Du har inte råd med det här svärdet.".
-# 9.  Fråga efter din karaktärs level (som ett tal). Om din level är 10 eller högre, skriv "Du kan lära dig formeln 'Fireball'!".
-# 10. Fråga efter ålder (som tal) och om de har en biljett (som text, "ja" eller "nej"). Om personen är över 17 OCH har en biljett ("ja"), skriv "Välkommen in på konserten!".
 # 11. Fråga om du har en "Guldnyckel" (ja/nej) eller en "Dyrk" (ja/nej). Om du har nyckel ("ja") ELLER dyrk ("ja"), skriv "Du lyckas öppna kistan!".
 # 12. Skapa en variabel är_förbannad = True. Om karaktären inte är förbannad, skriv "Dina attacker gör normal skada.". Annars, skriv "Dina attacker är svagare!".
 # 13. Fråga användaren efter deras namn. Om de skrev in ett namn (dvs. strängen inte är tom), skriv ut "Hej, [namn]!".
@@ -79,23 +72,77 @@
 # • Om den är över 25: Skriv ut "Det är sommarvarmt!".
 # • Om den istället är över 15: Skriv ut "Det är lagom svenskt väder.".
 # • Annars: Skriv ut "Det är kallt, ta på jackan!".
+# while True:
+#     try:
+#         temrature = int(input("What is temrature outside: "))
+#         if temrature>25:
+#             print("Det är sommarvarmt!")
+#             break
+#         elif temrature >15:
+#             print("Det är lagom svenskt väder.")
+#             break
+#         else:
+#             print("Det är kallt, ta på jackan!")
+#             break
+            
+#     except ValueError:
+#         print("Enter only digit")
+
+# 6.  Fråga användaren "Svara Ja eller Nej". Om svaret, oavsett versaler, är "ja", skriv ut "Du valde att fortsätta äventyret!".
+    
+# choice = input("Entr your choice(Ja/Nej): ")
+# if choice.lower() == "ja":
+#     print("Du valde att fortsätta äventyret!")
+# else:
+#     print("Tack för idag")
+# 7.  Fråga efter en spelares status (t.ex. "redo" eller "upptagen"). Om status inte är "redo", skriv "Vänta... Spelaren är inte redo.".
+# answer = input("redo eller upptagen: ")
+# if answer.lower() != "redo":
+#     print("Vänta... Spelaren är inte redo.")
+# else:
+#     print("Continue")
+
+# 8.  Fråga hur många guldmynt du har (som ett tal). Om du har färre än 50, skriv ut "Du har inte råd med det här svärdet.".
+# while True:
+#     try:
+#         guldmynt = int(input("Hur mänga uldmynt du har (som ett tal): "))
+#         if guldmynt <50:
+#             print("Du har inte råd med det här svärdet.")
+#             break
+#         else:
+#             print("Du har råd med svärdet")
+#             break
+#     except ValueError:
+#         print("Enter only digit")
+
+
+# 9.  Fråga efter din karaktärs level (som ett tal). Om din level är 10 eller högre, skriv "Du kan lära dig formeln 'Fireball'!".
+# while True:
+#     try:
+#         karaktärs_level = int(input("Hur är din karaktärs level (som ett tal): "))
+#         if  karaktärs_level >=10:
+#             print(f"Din karaktärs level är {karaktärs_level} ", end= " ")
+#             print("Du kan lära dig formeln 'Fireball'!")
+#             break
+#         else:
+#             print("Kom nästa gång")
+#             break
+#     except ValueError:
+#         print("Enter only digit")
+
+# 10. Fråga efter ålder (som tal) och om de har en biljett (som text, "ja" eller "nej"). Om personen är över 17 OCH har en biljett ("ja"), skriv "Välkommen in på konserten!".
+
 while True:
     try:
-        temrature = int(input("What is temrature outside: "))
-        if temrature>25:
-            print("Det är sommarvarmt!")
-            break
-        elif temrature >15:
-            print("Det är lagom svenskt väder.")
+        age = int(input("what is your age: "))
+        ticket = input("Do u have ticket (Yes/No)")
+        if age >17 and ticket.lower() == "yes":
+            print("Välkommen in på konserten!")
             break
         else:
-            print("Det är kallt, ta på jackan!")
+            print("You are not allowed")
             break
-            
     except ValueError:
-        print("Enter only digit")
-
-
-
-
+        print("Enter digit only")
+    
                   
